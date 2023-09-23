2f86bdebf0b37b4581826641c3127499787dc487
Luiz Gustavo S Lopes <lglopes1789@gmail.com>
2023-09-23 14:59:16 -0300

    luiz/ commit -2

--- tt/frontend/src/components/Aside.jsx
+++ tt/frontend/src/components/Aside.jsx
@@ -7,8 +7,64 @@ const Aside = () => {
   return (
     <>
         <aside>
-            <div id="men_lateral">
-            
+            <div id="logo">
+              <a href="#">
+                <img src="\src\imgs\logo.png" alt="logo" />
+              </a>
+            </div>
+            <div id="nav">
+              <div className="navbar">
+                <h2>Moedas</h2>
+                  <ul>
+                    <li>
+                      <a href='#'>
+                      <img src="\src\imgs\Doll.png"/>
+                      Dolar</a>
+                    </li>
+                    <li>
+                      <a href='#'>
+                      <img src="\src\imgs\Real.png"/>
+                      Real</a>
+                    </li>
+                    <li>
+                      <a href='#'>
+                      <img src="\src\imgs\Euro.png"/>
+                      Euro</a>
+                    </li>
+                    <li>
+                      <a href='#'>
+                      <img src="\src\imgs\Yen.png"/>
+                      Yen</a>
+                    </li>
+                    
+                  </ul>
+              </div>
+              <div className="navbar">
+                <h2>Moedas</h2>
+                  <ul>
+                    <li>
+                      <a href='#'>
+                      <img src="\src\imgs\Doll.png"/>
+                      Dolar</a>
+                    </li>
+                    <li>
+                      <a href='#'>
+                      <img src="\src\imgs\Real.png"/>
+                      Real</a>
+                    </li>
+                    <li>
+                      <a href='#'>
+                      <img src="\src\imgs\Euro.png"/>
+                      Euro</a>
+                    </li>
+                    <li>
+                      <a href='#'>
+                      <img src="\src\imgs\Yen.png"/>
+                      Yen</a>
+                    </li>
+                    
+                  </ul>
+              </div>
             </div>
         </aside>
     </>
