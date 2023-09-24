3e26071549bd5e8cc14536efc920943aa45c67b4
PedroMachado2022 <pedro.machado.rs@hotmail.com>
2023-09-24 15:02:01 -0300

    Pedro/RequestsAPI

--- tt/backend/main_or.py
+++ tt/backend/main_or.py
@@ -1,26 +1,62 @@
-import requests, pandas as pd
+import requests
+import pandas as pd
 
-entrada = input("DIGITE QUAL SEU DESEJO: ")
+# Função para gerar a Query
+def query_generator(lista: list[str]) -> list[str]:
+    listQuery: list[str] = []
+    # Loop para percorrer a lista de moedas a serem comparadas e criar a query com elas
+    for i in lista:
+        query = f"{moeda1.upper()}-{i}"
+        # Comparaçao Ex:. BRL-USD, BRL-EUR (formatadas para string)
+        listQuery.append(query)
+    return listQuery
 
 
-url = f"https://economia.awesomeapi.com.br/last/{entrada}"
-url2 = "https://economia.awesomeapi.com.br/json/daily/USD-BRL/15"
+## Moeda principal da query
+moeda1 = input("DIGITE QUAL SEU DESEJO: ")
+
+## Moeadas a serem comparadas
+list_moedas = ['BRL', 'USD', 'EUR']
+
+# Chamada da função que cria a lista BTC-BRL, BTC-USD, BTC-EUR
+list_query: list[str] = query_generator(list_moedas)
+print(list_query)
+
+
+# Query pronta para a URL.
+queryUrl: str = ",".join(list_query)
+print(queryUrl)
+
+# Query para comparação
+query_final: str = queryUrl.replace("-","").replace(',', " ")
+query_final_list: list[str] = query_final.split()
+print(query_final_list)
+
+url = f"https://economia.awesomeapi.com.br/last/{queryUrl}"
+
+#url2 = "https://economia.awesomeapi.com.br/json/daily/USD-BRL/15"
+
+# Requisição para a API
 response = requests.get(url)
 
-entrada2 = input("DIGITE QUAL SEU DESEJO: ")
 
 if response.status_code == 200:
-
+    # Pegando o json resposta da API
     data = response.json()
+    # Criaçao do Df através do Json resposta
     df = pd.DataFrame.from_dict(data, orient='index')
+    print(df)
+    
+    dfFinal = pd.DataFrame()
 
-    btc_data = df.loc[f'{entrada2}']
-
-    print(btc_data)
-    btc_dict = btc_data.to_dict()
-
-    for key, value in btc_dict.items():
-       print(f"Chave: {key}, Valor: {value}")
-
+    for i in query_final_list:
+        objData = df.loc[i]
+        objData_toDict = objData.to_dict()
+        dfFinal = dfFinal._append(objData_toDict, ignore_index=True)
+    print(df)  
 else:
-    print("Falha na solicitação à API. Código de status:", response.status_code)
+  print("Falha na solicitação à API. Código de status:", response.status_code)
+
+
+
+
