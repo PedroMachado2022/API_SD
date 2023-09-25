4c9b7fc3b89d5101358d0a403618c7e575584aad
PedroMachado2022 <pedro.machado.rs@hotmail.com>
2023-09-25 16:12:41 -0300

    Pedro/Organização

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+#aqui recebe moedas comentario muito foda okay?
+@app.route('/api/rota', methods=['POST'])
+def receber_post():
+    data = request.json 
+    resposta = {"mensagem": "Solicitação POST recebida com sucesso", "dados": data}
+    #funçao pra converçao (sem numero)
+    print(data['moeda1'], data['moeda2'])
+    return jsonify(resposta)
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Moedas from './Moedas'
+
+const Main = () => {
+  return (
+    <>
+        <main>
+           <Moedas/>
+        </main>
+    </>
+  )
+}
+
+export default Main
