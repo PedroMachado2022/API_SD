ddc0573eaa4260caa18acd885b01c08af86082de
Luiz Gustavo S Lopes <lglopes1789@gmail.com>
2023-09-26 14:54:27 -0300

    lopes/ teste

--- app.py
+++
@@ -1,4 +0,0 @@
-from backend.main import app
-
-if __name__ == "__main__":
-     app.run(debug=True)
--- backend/main.py
+++ backend/main.py
@@ -31,6 +31,6 @@ def queryGenerator_front(data):
    
     
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
