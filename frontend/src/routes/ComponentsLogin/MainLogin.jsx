c0486e291a048c9bd19d4ef177d3ca414c127a35
PedroMachado2022 <107653871+PedroMachado2022@users.noreply.github.com>
2023-12-03 20:21:38 -0300

    Pedro/ Login-page

--- frontend/src/routes/ComponentsLogin/MainLogin.jsx
+++ frontend/src/routes/ComponentsLogin/MainLogin.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import './MainLogin.css';
 
 const MainLogin = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode adicionar a lógica para enviar os dados do formulário
+    console.log('Dados do formulário:', formData);
+    // Resetar o formulário se necessário
+    setFormData({
+      name: '',
+      email: '',
+      password: '',
+    });
+  };
+
   return (
     <div>
-        exemplo
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default MainLogin
+export default MainLogin;
